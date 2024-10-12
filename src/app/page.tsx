49606de09a63b6152297862fb6e37c44fc1dde51
Ekaraja <melisakmnv@gmail.com>
2024-10-12 18:50:26 +0200

    added User Menu component and Logout properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,14 @@
-"use client"
 
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current"
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { UserButton } from "@/features/auth/components/user-button";
 
 
 export default function Home() {
 
-    const router = useRouter()
-    const { data, isLoading } = useCurrent();
-
-    const { mutate } = useLogout();
-
-    useEffect(() => {
-
-        if (!data && !isLoading) {
-            router.push("/sign-in")
-        }
-    }, [data])
+    
 
     return (
         <div>
-            <Button onClick={() => mutate()}>Logout</Button>
-
-            Only visible to authorized users
-
-
+            <UserButton />
         </div>
     )
 }
