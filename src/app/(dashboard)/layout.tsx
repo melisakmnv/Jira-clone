a8151451190e52c08d66ff3c48f560b35a4db6c8
Ekaraja <melisakmnv@gmail.com>
2024-10-13 17:42:09 +0200

    added navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 
 
@@ -14,9 +15,9 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
                     <Sidebar/>
                 </div>
 
-                <div className="lg:pl-[264px]">
+                <div className="lg:pl-[264px] w-full">
                     <div className="mx-auto max-w-screen-2xl h-full">
-                        {/* TODO : Navbar */}
+                        <Navbar/>
                         <main className="h-full py-8 px-6 flex flex-col">
                             {children}
                         </main>
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
 
     return (
         <div>
-            <UserButton />
+           THis is a home page
         </div>
     );
 };
